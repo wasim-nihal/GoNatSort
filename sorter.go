84587a00ea8c9951b81f8afffd659df6fc33629d
Nihal <38865967+wasim-nihal@users.noreply.github.com>
2024-04-23 16:38:18 +0530

    Update sorter.go

--- sorter.go
+++ sorter.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Syed Wasim Nihal (wasim-nihal). All rights reserved.
+// Copyright 2024 Syed Wasim Nihal (wasim-nihal). All rights reserved.
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 package gonatsort
